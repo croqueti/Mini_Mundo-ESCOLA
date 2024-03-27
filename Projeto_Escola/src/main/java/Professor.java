40956d5214ac7428f1ebef93162ef6206d96568b
Vi <108130537+croqueti@users.noreply.github.com>
2024-03-27 11:17:15 -0300

    Add files via upload

---
+++ Projeto_Escola/src/main/java/Professor.java
@@ -0,0 +1,42 @@
+//Estefani -- Classe Professor 2
+public class Professor {
+    private String nome;
+    private String materia;
+    private int idade;
+
+    public Professor() {
+    }
+
+    public Professor(String nome, String materia, int idade) {
+        this.nome = nome;
+        this.materia = materia;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public static void main(String[] args) {
+        // 
+    }
+}
